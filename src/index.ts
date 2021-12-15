2dd8b94479e800c95730a0ded495e3eb33f5ed10
Falaxir <falaxir@gmail.com>
2021-12-15 17:13:33 +0100

    Added file send to index, search queries

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,15 @@ import session from 'express-session'
 import dotenv from 'dotenv'
 // require('./search_engine')
 import {searchData, indexDoc, createIndex, putPipeline} from "./search_engine";
+import e from "express";
+import multer from "multer";
+import * as os from "os";
 
 dotenv.config({ path: 'backend.env' });
 dotenv.config({ path: 'db.env' });
 
+const fileUpload = require('express-fileupload');
+const upload = multer({ dest: os.tmpdir() });
 const app = express()
 const port = process.env.PORT || 3000
 
@@ -20,6 +25,12 @@ app.use(session({
     saveUninitialized: true
 }))
 app.use(cookieParser(process.env.SESSION_SECRET))
+app.use(fileUpload({
+    createParentPath: true,
+    limits: {
+        fileSize: 10 * 1024 * 1024 * 1024 //2MB max file(s) size
+    },
+}));
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
@@ -27,12 +38,42 @@ app.get('/', (req, res) => {
 
 //Search on all index files
 app.get('/search', async (req, res) => {
-    const a = await searchData('true', 'researchshare');
+    await searchData(req.query.text, 'researchshare').then(value => {
+        res.send(value);
+    }).catch(e => {
+        res.send({result: "error"});
+    });
 })
 
 //Add a file to index
-app.get('/index', async (req, res) => {
-    const a = await indexDoc('tsconfig.json', 'researchshare');
+app.post('/index_file', upload.single('file'), async (req, res) => {
+    try {
+        if(!req.files) {
+            res.send({
+                status: false,
+                message: 'No file uploaded'
+            });
+        } else {
+            console.log("requ")
+            //Use the name of the input field (i.e. "avatar") to retrieve the uploaded file
+            let toindex = req.files["file"];
+
+            //Use the mv() method to place the file in upload directory (i.e. "uploads")
+            toindex.mv('/tmp/' + toindex.name);
+
+            //send response
+            await indexDoc(os.tmpdir() + '/' + toindex.name, 'researchshare').then(value => {
+                console.log(value);
+                res.send(value);
+            }).catch(e => {
+                console.log("fail")
+                console.log(e);
+                res.send({result: "error"});
+            })
+        }
+    } catch (err) {
+        res.status(500).send(err);
+    }
 })
 
 //This command first to initialise
@@ -44,5 +85,5 @@ app.get('/initialise', async (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log(`server is running on freaking port ${port}`)
+    console.log(`server is running on this port ${port}`)
 })
